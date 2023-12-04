962186e893b645e722674d125ebb5708835e396c
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-12-04 01:37:29 +0000

    updated comments

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import os
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
+#The purpose of this basic project is to  select the shortest StackOverflow questions containing "hello" and "world".
+
+# Authentication: create a service account, generate a key in JSON format 
+# Next save its contents (everything except opening `{` and closing `}`) as google_key secret.
+
 # how-to: https://docs.aws.amazon.com/dms/latest/sbs/bigquery-redshift-migration-step-1.html (steps 1.-10. on one web page)
 # thanks: https://stackoverflow.com/questions/73195754/python-google-bigquery-how-to-authenticate-without-json-file
-cred = service_account.Credentials.from_service_account_info(eval("{" + os.environ['google_oauth'] +"}"))
+cred = service_account.Credentials.from_service_account_info(eval("{" + os.environ['google_key'] +"}"))
 client = bigquery.Client(credentials=cred, project=cred.project_id)
 
 # SQL dialect reference: https://cloud.google.com/bigquery/docs/reference/standard-sql/string_functions
